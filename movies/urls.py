e0c67c9e7664cdc10f39512a1882ab16aa101462
Juyeong Lee <2jy22@naver.com>
2021-02-05 04:40:49 +0900

    feat: naver movie search api

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
   path('<str:movie_name>/', search, name="search"),
-  path('detail/<int:movie_cd>/', detail, name="detail")
+  path('detail/<int:movie_cd>/', detail, name="detail"),
+  path('image/<str:movie_name>/', image, name="image"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from movies.serializers import *
 import urllib.request as ul
-import os, json
+import os, json, ssl
 from urllib.parse import quote
 from pathlib import Path
 
@@ -14,11 +14,35 @@ def get_api_key(type):
   CREDENTIAL_FILE = os.path.join(BASE_DIR, 'credentials.json')
   credentials = json.loads(open(CREDENTIAL_FILE).read())
 
-  if type is 'detail':
-    key = credentials["MOVIE_DETAIL_API_KEY"]
+  if type is 'image':
+    return credentials["NAVER_CLIENT_ID"], credentials["NAVER_CLIENT_SECRET"]
+  elif type is 'detail':
+    return credentials["MOVIE_DETAIL_API_KEY"]
   else:
-    key = credentials["MOVIE_API_KEY"]
-  return key
+    return credentials["MOVIE_API_KEY"]
+
+
+# 영화 이미지
+@api_view(['GET'])
+def image(request, movie_name):
+  client_id, client_secret = get_api_key("image")
+  query = quote(movie_name)
+
+  url = "https://openapi.naver.com/v1/search/movie.json?query={}".format(query)
+  request = ul.Request(url)
+  request.add_header("X-Naver-Client-Id", client_id)
+  request.add_header("X-Naver-Client-Secret",client_secret)
+  context = ssl._create_unverified_context()
+
+  response = ul.urlopen(request, context=context)
+  rescode = response.getcode()
+
+  if (rescode == 200):
+    response_data = json.loads(response.read())
+    return Response(data=response_data)
+  else:
+    print("Error code: " + rescode)
+
 
 # 영화목록
 @api_view(['GET'])
@@ -32,9 +56,12 @@ def search(request, movie_name):
   response = ul.urlopen(request)
   rescode = response.getcode()
 
-  if(rescode == 200):
+  if (rescode == 200):
     response_data = json.loads(response.read())
     return Response(data=response_data)
+  else:
+    print("Error code: " + rescode)
+
 
 # 영화 상세정보
 @api_view(['GET'])
@@ -48,6 +75,8 @@ def detail(request, movie_cd):
   response = ul.urlopen(request)
   rescode = response.getcode()
 
-  if(rescode == 200):
+  if (rescode == 200):
     response_data = json.loads(response.read())
     return Response(data=response_data)
+  else:
+    print("Error code: " + rescode)
