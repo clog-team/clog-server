d7e2d1817d6f2c5741abd2c4808f7083c7bdb09d
Juyeong Lee <2jy22@naver.com>
2021-02-05 06:32:32 +0900

    json 조립

--- movies/views.py
+++ movies/views.py
@@ -7,6 +7,7 @@ import urllib.request as ul
 import os, json, ssl
 from urllib.parse import quote
 from pathlib import Path
+from django.http import HttpRequest, HttpResponse
 
 # 영화진흥위원회 api key
 def get_api_key(type):
@@ -22,7 +23,7 @@ def get_api_key(type):
     return credentials["MOVIE_API_KEY"]
 
 
-# 영화 이미지
+# 영화 이미지 url
 @api_view(['GET'])
 def image(request, movie_name):
   client_id, client_secret = get_api_key("image")
@@ -39,8 +40,8 @@ def image(request, movie_name):
 
   if (rescode == 200):
     response_data = json.loads(response.read())
-    image_url = response_data["items"][0]["image"] # Note: 첫 번째 검색 결과의 사진을 가져옴
-    return Response(data=image_url)
+    image_url = json.dumps(response_data["items"][0]["image"]) # Note: 첫 번째 검색 결과의 사진을 가져옴
+    return HttpResponse(image_url, content_type="text/json-comment-filtered")
   else:
     print("Error code: " + rescode)
 
@@ -52,21 +53,38 @@ def search(request, movie_name):
   query = quote(movie_name)
 
   url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={}&movieNm={}".format(key, query)
-  request = ul.Request(url)
+  url_request = ul.Request(url)
 
-  response = ul.urlopen(request)
+  response = ul.urlopen(url_request)
   rescode = response.getcode()
 
   if (rescode == 200):
     response_data = json.loads(response.read())
-    return Response(data=response_data)
+
+    # json 재구성
+    items = []
+    movie_list = response_data["movieListResult"]["movieList"]
+
+    for i in range(response_data["movieListResult"]["totCnt"]):
+      movie = {}
+      movie["movieCode"] = movie_list[i]["movieCd"]
+      movie["thumbnailUrl"] = json.loads(image(request._request, movie_list[i]["movieNm"]).content)
+      movie["movie_name"] = movie_list[i]["movieNm"]
+      movie["directors"] = movie_list[i]["directors"]
+      movie["opening_date"] = movie_list[i]["openDt"]
+      movie["genre"] = movie_list[i]["repGenreNm"]
+      movie["running_time"] = int(json.loads(detail(request._request, movie_list[i]["movieCd"], 'running_time').content))
+      items.append(movie)
+
+    ret_json_obj = {"items": items}
+    return Response(data=ret_json_obj)
   else:
     print("Error code: " + rescode)
 
 
 # 영화 상세정보
 @api_view(['GET'])
-def detail(request, movie_cd):
+def detail(request, movie_cd, running_time=''):
   key = get_api_key("detail")
   
   url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key={}&movieCd={}".format(key, movie_cd)
@@ -78,6 +96,12 @@ def detail(request, movie_cd):
 
   if (rescode == 200):
     response_data = json.loads(response.read())
-    return Response(data=response_data)
+
+    if running_time is 'running_time': # 상영시간 정보
+      time = json.dumps(response_data["movieInfoResult"]["movieInfo"]["showTm"])
+      return HttpResponse(time, content_type="text/json-comment-filtered")
+    else: # 상세정보 전체
+      print('not running time')
+      return Response(data=response_data)
   else:
     print("Error code: " + rescode)
