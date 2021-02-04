abce09cf73853c571cda581c1b7ae4065da28d28
Juyeong Lee <2jy22@naver.com>
2021-02-05 04:22:13 +0900

    feat: movie detail api

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
   path('<str:movie_name>/', search, name="search"),
+  path('detail/<int:movie_cd>/', detail, name="detail")
 ]
--- movies/views.py
+++ movies/views.py
@@ -8,21 +8,41 @@ import os, json
 from urllib.parse import quote
 from pathlib import Path
 
-# 영화목록 api key
-def search_list_key():
+# 영화진흥위원회 api key
+def get_api_key(type):
   BASE_DIR = Path(__file__).resolve().parent.parent
   CREDENTIAL_FILE = os.path.join(BASE_DIR, 'credentials.json')
   credentials = json.loads(open(CREDENTIAL_FILE).read())
-  key = credentials["MOVIE_API_KEY"]
+
+  if type is 'detail':
+    key = credentials["MOVIE_DETAIL_API_KEY"]
+  else:
+    key = credentials["MOVIE_API_KEY"]
   return key
 
 # 영화목록
 @api_view(['GET'])
 def search(request, movie_name):
-  key = search_list_key()
+  key = get_api_key("list")
   query = quote(movie_name)
 
   url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={}&movieNm={}".format(key, query)
+  request = ul.Request(url)
+
+  response = ul.urlopen(request)
+  rescode = response.getcode()
+
+  if(rescode == 200):
+    response_data = json.loads(response.read())
+    return Response(data=response_data)
+
+# 영화 상세정보
+@api_view(['GET'])
+def detail(request, movie_cd):
+  key = get_api_key("detail")
+  
+  url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key={}&movieCd={}".format(key, movie_cd)
+
   request = ul.Request(url)
 
   response = ul.urlopen(request)
