f60410ce7a4f9ebba561ed87260cd77329e1d6cb
Juyeong Lee <2jy22@naver.com>
2021-02-05 11:19:56 +0900

    profile & badge migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+# User 모델과 대응하는 프로필(프로필 사진을 등록하기 위함)
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  image = models.ImageField(verbose_name="Image of User", upload_to="image/", default="movie-it-logo.png")
+
+# 뱃지
+class Badge(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
