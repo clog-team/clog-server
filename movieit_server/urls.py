809358603d208f6ea27c3b3b8a607f5852c05e6d
Juyeong Lee <2jy22@naver.com>
2021-02-05 12:50:38 +0900

    fix typo

--- movieit_server/urls.py
+++ movieit_server/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('api-auth/', include("rest_framework.urls")),
     path('rest-auth/', include("rest_auth.urls")),
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
-    path('usuers/', include('users.urls')),
+    path('users/', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
