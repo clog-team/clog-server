2496781d4bc1a1541d913e018a45b1c1d17b793e
Juyeong Lee <2jy22@naver.com>
2021-02-05 15:00:49 +0900

    remove comment

--- movieit_server/urls.py
+++ movieit_server/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('api-auth/', include("rest_framework.urls")),
     path('rest-auth/', include("rest_auth.urls")),
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
-    # path('users/', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
